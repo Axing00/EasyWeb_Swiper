7f59234ebae36b8c7f31c96673a86513fa36ad53
ellie <gyeongjin.hong@dailyh.org>
2023-06-07 09:57:31 +0900

    + swiper thumbs 완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /*eslint-disable*/
 import React, { Component } from "react";
-import Myslide from "./Myslide";
+// import Myslide from "./Myslide";
+import MyslideThumbs from "./Myslide_thumbs";
 
 import "./App.scss";
 
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Myslide></Myslide>
+        <MyslideThumbs></MyslideThumbs>
       </div>
     );
   }
--- src/Myslide_thumbs.js
+++ src/Myslide_thumbs.js
@@ -1,14 +1,15 @@
 /*eslint-disable*/
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar } from "swiper";
+import { Navigation, Pagination, Thumbs } from "swiper";
 
 // Import Swiper styles
 import "swiper/scss";
+import "swiper/scss/free-mode";
 import "swiper/scss/pagination";
 import "swiper/scss/navigation";
-import "swiper/scss/scrollbar";
+import "swiper/scss/thumbs";
 
 import prevButtonImage from "./imgs/button/chevron-left.svg";
 import nextButtonImage from "./imgs/button/chevron-right.svg";
@@ -34,32 +35,57 @@ const data = [
     imgs: "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
     alt: "슬라이드이미지4",
   },
+  {
+    id: 5,
+    imgs: "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+    alt: "슬라이드이미지4",
+  },
 ];
 
 export default () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar]}
-      spaceBetween={20}
-      slidesPerView={3}
-      navigation={{
-        prevEl: `.swiper-button-prev`,
-        nextEl: `.swiper-button-next`,
-      }}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-    >
-      <div className="swiper-button-prev">
-        <img src={prevButtonImage} alt="Previous" />
-      </div>
-      <div className="swiper-button-next">
-        <img src={nextButtonImage} alt="Next" />
-      </div>
-      {data.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <img src={slide.imgs} alt={slide.alt} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Thumbs]}
+        onSwiper={setThumbsSwiper}
+        freeMode={true}
+        watchSlidesProgress={true}
+        spaceBetween={20}
+        slidesPerView={4}
+        className="mySwiper_thumbs"
+      >
+        {data.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <img src={slide.imgs} alt={slide.alt} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper
+        modules={[Navigation, Pagination, Thumbs]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation={{
+          prevEl: `.swiper-button-prev`,
+          nextEl: `.swiper-button-next`,
+        }}
+        pagination={{ clickable: true }}
+        thumbs={{ swiper: thumbsSwiper }}
+        className="mySwiper_cont"
+      >
+        <div className="swiper-button-prev">
+          <img src={prevButtonImage} alt="Previous" />
+        </div>
+        <div className="swiper-button-next">
+          <img src={nextButtonImage} alt="Next" />
+        </div>
+        {data.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <img src={slide.imgs} alt={slide.alt} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
