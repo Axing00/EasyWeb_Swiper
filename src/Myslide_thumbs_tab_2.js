3d983be89b800bae552b13c136a812322b51a478
ellie <gyeongjin.hong@dailyh.org>
2023-06-08 17:35:18 +0900

    + 스타일 적용중

--- src/Myslide_thumbs_tab_2.js
+++ src/Myslide_thumbs_tab_2.js
@@ -14,6 +14,8 @@ import "swiper/scss/thumbs";
 // import img
 import plus from "./imgs/button/plus.svg";
 import x_icon from "./imgs/button/x.svg";
+import EU_flag from "./imgs/flag/EU.svg";
+import US_flag from "./imgs/flag/US.svg";
 
 export default () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -62,8 +64,8 @@ export default () => {
           >
             <SwiperSlide>
               <a href="#none" title="">
-                <div className="sd_top">
-                  <p className="btn_condition btn_red">
+                <div className="sd_top mg_0408">
+                  <p className="btn_condition btn_red mg_08">
                     적극 매수
                     <span className="plus_icon">
                       <img src={plus} alt="plus_icon" />
@@ -75,7 +77,46 @@ export default () => {
                     </figure>
                   </button>
                 </div>
-                <div className="sd_middle"></div>
+                <div className="sd_middle">
+                  <div className="event_name">
+                    <div className="flag_box">
+                      <figure>
+                        <img src={EU_flag} alt="유럽연합" />
+                      </figure>
+                      <figure>
+                        <img src={US_flag} alt="미국" />
+                      </figure>
+                    </div>
+                    <div className="title_box">
+                      <p className="_nation">
+                        EUR
+                        <span className="__bar">/</span>
+                        USD
+                      </p>
+                      <p className="_unit">
+                        유로
+                        <span className="__bar">/</span>
+                        달러
+                      </p>
+                    </div>
+                  </div>
+                  <div className="rates_area mg_t08">
+                    <p className="bid">1.072</p>
+                    <div className="rates_change">
+                      <p>
+                        <i className="up xi-caret-up"></i>
+                        <i className="down xi-caret-down icon_hide"></i>
+                        <span>0.0038</span>
+                      </p>
+                      <p>
+                        <i className="pluse xi-plus-min"></i>
+                        <i className="minus xi-minus-min icon_hide"></i>
+                        <span>0.36%</span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="graph_area"></div>
+                </div>
               </a>
             </SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
@@ -87,8 +128,8 @@ export default () => {
           <Swiper
             spaceBetween={50}
             slidesPerView={3}
-            nested={true} // 중첩된 슬라이더를 지원하기 위해 nested prop 추가
-            key="nested-swiper" // 각각의 중첩된 슬라이더에 고유한 key prop 추가
+            nested={true}
+            key="nested-swiper"
           >
             <SwiperSlide>Slide 5</SwiperSlide>
             <SwiperSlide>Slide 6</SwiperSlide>
